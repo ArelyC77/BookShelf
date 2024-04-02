3a77a8ca3d70281084aa8525c0fc85d89c7999f2
Karina <kmora048@fiu.edu>
2024-04-01 22:43:38 -0400

    hard code json

---
+++ lab-insta-parse/Book/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  MovieCo
+//
+//  Created by Arely Correa on 2/13/24.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
